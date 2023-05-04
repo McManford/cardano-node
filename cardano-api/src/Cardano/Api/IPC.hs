8ab9035c4ba67c0bc41c6ed28a55e956b25a5e1d
Jordan Millar <jordan.millar@iohk.io>
2023-05-04 13:33:11 -0400

    Expose needed internals from Cardano.Api.IPC

--- cardano-api/src/Cardano/Api/IPC.hs
+++ cardano-api/src/Cardano/Api/IPC.hs
@@ -21,11 +21,11 @@ module Cardano.Api.IPC (
     connectToLocalNodeWithVersion,
     LocalNodeConnectInfo(..),
     localConsensusMode,
-    LocalNodeClientParams(..),
     mkLocalNodeClientParams,
     LocalNodeClientProtocols(..),
     LocalChainSyncClient(..),
     LocalNodeClientProtocolsInMode,
+
 
     -- ** Modes
     -- | TODO move to Cardano.Api
@@ -53,9 +53,10 @@ module Cardano.Api.IPC (
     -- *** Local state query
     LocalStateQueryClient(..),
     AcquiringFailure(..),
+    NodeToClientVersion(..),
     QueryInMode(..),
     QueryInEra(..),
-    QueryInShelleyBasedEra(..),
+    QueryShelleyBasedEra(..),
     queryNodeLocalState,
 
     -- *** Local tx monitoring
@@ -77,9 +78,15 @@ module Cardano.Api.IPC (
     consensusModeOnly,
     toAcquiringFailure,
 
-    NodeToClientVersion(..),
 
-    UnsupportedNtcVersionError(..),
+    -- *** Internals
+    LocalNodeClientProtocolsForBlock(..),
+    LocalNodeClientParams(..),
+    convLocalChainSyncClientPipelined,
+    convLocalChainSyncClient,
+    convLocalTxSubmissionClient,
+    convLocalTxMonitoringClient,
+
   ) where
 
 import           Data.Void (Void)
@@ -129,11 +136,11 @@ import qualified Ouroboros.Consensus.Shelley.Ledger.Block as Consensus
 import           Cardano.Api.Block
 import           Cardano.Api.HasTypeProxy
 import           Cardano.Api.InMode
-import           Cardano.Api.IPC.Version
 import           Cardano.Api.Modes
 import           Cardano.Api.NetworkId
 import           Cardano.Api.Protocol
 import           Cardano.Api.Query
+import           Cardano.Api.Query.ShelleyBased
 import           Cardano.Api.Tx (getTxBody)
 import           Cardano.Api.TxBody
 
@@ -397,7 +404,6 @@ data LocalNodeClientProtocolsForBlock block =
                                         SlotNo IO ())
      }
 
-
 -- | Convert from the mode-parametrised style to the block-parametrised style.
 --
 mkLocalNodeClientParams :: forall mode block.
@@ -527,7 +533,6 @@ convLocalStateQueryClient mode =
       toConsensusQuery
       fromConsensusQueryResult
 
-
 --TODO: Move to consensus
 mapLocalTxMonitoringClient
   :: forall txid txid' tx tx' m a. Functor m
@@ -571,10 +576,6 @@ mapLocalTxMonitoringClient convTxid convTx ltxmc =
 -- | Establish a connection to a node and execute a single query using the
 -- local state query protocol.
 --
-
-data AcquiringFailure = AFPointTooOld
-                      | AFPointNotOnChain
-                      deriving (Eq, Show)
 
 toAcquiringFailure :: Net.Query.AcquireFailure -> AcquiringFailure
 toAcquiringFailure AcquireFailurePointTooOld = AFPointTooOld
